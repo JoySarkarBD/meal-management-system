c6bb39337698399793a52bae19dd3d41dcf3f626
JoySarkarBD <developer.joysarkar@gmail.com>
2023-10-11 17:09:22 +0600

    meal and user controller mockup ready and mealRoutes ready

--- server/src/routes/mealRoutes.js
+++ server/src/routes/mealRoutes.js
@@ -18,4 +18,11 @@ router.put("/meals/:id", MealController.updateMeal);
 // Delete a meal (Admin)
 router.delete("/meals/:id", MealController.deleteMeal);
 
+// Reserve meals for the next day until 6 PM (User)
+router.post("/meals/reserve", MealController.reserveMeals);
+
+// List and cancel bookings for the next day before 6 PM (User)
+router.get("/meals/bookings", MealController.getUserBookings);
+router.delete("/meals/bookings/:id", MealController.cancelBooking);
+
 module.exports = router;
