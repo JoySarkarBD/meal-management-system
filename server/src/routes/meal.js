4af305516c5448e3cda566060451d23bf4c2ccb8
hasan1119 <contact@rokibul.com>
2023-10-15 12:30:31 +0600

    'update'

--- server/src/routes/meal.js
+++ server/src/routes/meal.js
@@ -1,20 +1,20 @@
 // Import necessary controllers
-const {
-  createMeal,
-  getAllMeals,
-  getMealById,
-  updateMeal,
-  deleteMeal,
-} = require("../controllers/MealController");
+// const {
+//   createMeal,
+//   getAllMeals,
+//   getMealById,
+//   updateMeal,
+//   deleteMeal,
+// } = require("../controllers/MealController");
 
 const express = require("express");
 const router = express.Router();
 
 // Meal Routes
-router.post("/meals", createMeal);
-router.get("/meals", getAllMeals);
-router.get("/meals/:id", getMealById);
-router.put("/meals/:id", updateMeal);
-router.delete("/meals/:id", deleteMeal);
+// router.post("/meals", createMeal);
+// router.get("/meals", getAllMeals);
+// router.get("/meals/:id", getMealById);
+// router.put("/meals/:id", updateMeal);
+// router.delete("/meals/:id", deleteMeal);
 
 module.exports = router;
